354cbd197cca5a6708360e0d6136ebe2eb9b895c
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-06-10 00:06:01 -0400

    Index `Grant` events

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -15,12 +15,14 @@ export default createSchema((p) => ({
     description: p.string().optional(),
     memberships: p.many('Membership.collectionId'),
     purchaseEvents: p.many('PurchaseEvent.collectionId'),
+    grantEvents: p.many('GrantEvent.collectionId'),
   }),
 
   Membership: p.createTable({
     id: p.string(), // collectionId:account
     account: p.hex(),
     purchaseEvents: p.many('PurchaseEvent.membershipId'),
+    grantEvents: p.many('GrantEvent.membershipId'),
     collectionId: p.hex().references('Collection.id'),
     collection: p.one('collectionId'),
     expiresAt: p.bigint(),
@@ -39,6 +41,20 @@ export default createSchema((p) => ({
     tokensTransferred: p.bigint(),
     timePurchased: p.bigint(),
     rewardPoints: p.bigint(),
+    expiresAt: p.bigint(),
+    timestamp: p.bigint(),
+  }),
+
+  GrantEvent: p.createTable({
+    id: p.string(),
+    chainId: p.int(),
+    membershipId: p.string().references('Membership.id'),
+    membership: p.one('membershipId'),
+    collectionId: p.hex().references('Collection.id'),
+    collection: p.one('collectionId'),
+    account: p.hex(),
+    tokenId: p.bigint(),
+    secondsGranted: p.bigint(),
     expiresAt: p.bigint(),
     timestamp: p.bigint(),
   }),
--- src/STPv1.ts
+++ src/STPv1.ts
@@ -2,9 +2,7 @@ import { ponder } from '@/generated'
 
 ponder.on('STPv1:Purchase', async ({ event, context }) => {
   const { Collection, Membership, PurchaseEvent } = context.db
-
   const { account, tokensTransferred, rewardPoints, expiresAt } = event.args
-
   const collectionId = event.log.address
   const membershipId = `${collectionId}:${account}`
 
@@ -38,6 +36,37 @@ ponder.on('STPv1:Purchase', async ({ event, context }) => {
       totalRewardPoints: current.totalRewardPoints + rewardPoints,
       totalCreatorEarnings: current.totalCreatorEarnings + tokensTransferred,
     }),
+  })
+})
+
+ponder.on('STPv1:Grant', async ({ event, context }) => {
+  const { GrantEvent, Membership } = context.db
+  const { account, expiresAt } = event.args
+  const collectionId = event.log.address
+  const membershipId = `${collectionId}:${account}`
+
+  await GrantEvent.create({
+    id: event.log.id,
+    data: {
+      ...event.args,
+      membershipId,
+      chainId: context.network.chainId,
+      collectionId,
+      timestamp: event.block.timestamp,
+    },
+  })
+
+  await Membership.upsert({
+    id: membershipId,
+    create: {
+      account,
+      collectionId,
+      expiresAt,
+      createdAt: event.block.timestamp,
+    },
+    update: {
+      expiresAt: expiresAt,
+    },
   })
 })
 
